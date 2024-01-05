7c43628f932b4f2bf5d5a2e8900ba6ad8507b402
Atharva Sonawane <111640636+comet44@users.noreply.github.com>
2024-01-05 23:27:20 +0530

    Update membercountermeta.py

--- membercountermeta.py
+++ membercountermeta.py
@@ -1,3 +1,4 @@
+
 import os
 from pyrogram import Client, filters
 from pyrogram.types import Message
@@ -26,43 +27,43 @@ C_MESSAGE_ID = int(os.environ["C_MESSAGE_ID"])
 
 
 
-print(text_1)
 async def main_MemberCounterMeta():
     async with MemberCounterMeta:
         try:
             while True:
                 print(text_2)
-                edit_message_text_teletips = "**📈 | Real-Time Member Counter** "
+                
 
                 for CHANNEL_OR_GROUP in CHANNEL_OR_GROUP_LIST:
                     try:
-                        
-                time = datetime.now(pytz.timezone(f"{TIME_ZONE}"))
-                last_update = time.strftime(f"%Y-%m-%d %H:%M:%S")
-                xxx_teletips += f"\n\n⌛️ Last checked on: {last_update} ({TIME_ZONE})\n\n<i>♻️ Refreshes automatically Every 45 Minutues</i>"
-                await MemberCounterMeta.edit_message_text(int(BOT_CHANNEL_OR_GROUP_ID), BOT_MESSAGE_ID, xxx_teletips)
-                print(f"Last checked on: {last_update}")
-                await MemberCounterMeta.send_message(int(bot_admin_id), f"Last checked on: {last_update}")
-                await asyncio.sleep(7)
-                print(f"trying to do countdown")
-                desired_timezone_c = 'Asia/Kolkata'
-                target_date = datetime(2024, 5, 4, 23, 59, 59 , tzinfo=pytz.timezone(desired_timezone_c))
-                current_time_c = datetime.now(pytz.timezone(desired_timezone_c))
-                remaining_time = target_date - current_time_c  # Use 'Asia/Kolkata' for Indian Standard Time
-               
-                if remaining_time.total_seconds() <= 0:
-                    print("Countdown reached zero.")
-                else: 
-                    total_seconds = remaining_time.total_seconds()
-                    days, seconds_remaining = divmod(total_seconds, 86400)
-                    hours, seconds_remaining = divmod(seconds_remaining, 3600)
-                    minutes, seconds = divmod(seconds_remaining, 60)
-                countdown_message = (f"🌀**COUNTDOWN FOR NEET 2024 to 5 May, 2024** \n\n **Time Left**: {days} days, {hours} hours, {minutes} minutes, {seconds} seconds \n\n  \n\n <i>♻️ Refreshes automatically  </i>")
-                await MemberCounterMeta.edit_message_text(int(BOT_CHANNEL_OR_GROUP_ID), C_MESSAGE_ID, countdown_message)
-                print(f"COUNTDOWN FOR NEET 2024 to 5 May, 2024 \n\n Time Left: {days} days, {hours} hours, {minutes} minutes, {seconds} seconds \n\n  ")
-                await asyncio.sleep(10)  # 15 minutes = 900 seconds # 15 minutes = 900 seconds
-        except FloodWait as e:
-            await asyncio.sleep(e.x)
+                        time = datetime.now(pytz.timezone(f"{TIME_ZONE}"))
+                        last_update = time.strftime(f"%Y-%m-%d %H:%M:%S")
+                        xxx_teletips += f"\n\n⌛️ Last checked on: {last_update} ({TIME_ZONE})\n\n<i>♻️ Refreshes automatically Every 45 Minutues</i>"
+                        await MemberCounterMeta.edit_message_text(int(BOT_CHANNEL_OR_GROUP_ID), BOT_MESSAGE_ID, xxx_teletips)
+                        print(f"Last checked on: {last_update}")
+                        await MemberCounterMeta.send_message(int(bot_admin_id), f"Last checked on: {last_update}")
+                        await asyncio.sleep(7)
+                        print(f"trying to do countdown")
+                        desired_timezone_c = 'Asia/Kolkata'
+                        target_date = datetime(2024, 5, 4, 23, 59, 59 , tzinfo=pytz.timezone(desired_timezone_c))
+                        current_time_c = datetime.now(pytz.timezone(desired_timezone_c))
+                        remaining_time = target_date - current_time_c  # Use 'Asia/Kolkata' for Indian Standard Time
+
+                        if remaining_time.total_seconds() <= 0:
+                            print("Countdown reached zero.")
+                        else: 
+                            total_seconds = remaining_time.total_seconds()
+                            days, seconds_remaining = divmod(total_seconds, 86400)
+                            hours, seconds_remaining = divmod(seconds_remaining, 3600)
+                            minutes, seconds = divmod(seconds_remaining, 60)
+                        countdown_message = (f"🌀**COUNTDOWN FOR NEET 2024 to 5 May, 2024** \n\n **Time Left**: {days} days, {hours} hours, {minutes} minutes, {seconds} seconds \n\n  \n\n <i>♻️ Refreshes automatically  </i>")
+                        await MemberCounterMeta.edit_message_text(int(BOT_CHANNEL_OR_GROUP_ID), C_MESSAGE_ID, countdown_message)
+                        print(f"COUNTDOWN FOR NEET 2024 to 5 May, 2024 \n\n Time Left: {days} days, {hours} hours, {minutes} minutes, {seconds} seconds \n\n  ")
+                        await asyncio.sleep(10)  # 15 minutes = 900 seconds # 15 minutes = 900 seconds
+                    except FloodWait as e:
+                        await asyncio.sleep(e.x)
+        except Exception as e:
+            print(f"An error occurred: {e}")
 @MemberCounterMeta.on_message(filters.command("status", "!") & filters.me)
 async def alive(_, message: Message):
     await message.edit("Your MemberCounter is alive!")
